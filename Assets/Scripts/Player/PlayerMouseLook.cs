33f6eedaf3e255cb2775857993b35dceccf53f1f
Barnabás BARTHA <barnabas@barthaweb.com>
2021-02-08 18:00:03 +0100

    Cleanup

--- Assets/Scripts/Player/PlayerMouseLook.cs
+++ Assets/Scripts/Player/PlayerMouseLook.cs
@@ -1,38 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMouseLook : MonoBehaviour
-{
-
-    [SerializeField] float mouseSensitivity = 100f;
-    public Transform player;
-    private float xRotation = 0f;
+public class PlayerMouseLook : MonoBehaviour {
+   [SerializeField] float mouseSensitivity = 100f;
+   public Transform player;
+   private float xRotation;
 
 
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-    }
+   void Start() {
+      Cursor.lockState = CursorLockMode.Locked;
+   }
 
-    void Update()
-    {
-        UpdateRotation();
-    }
+   void Update() {
+      UpdateRotation();
+   }
 
-    private void UpdateRotation()
-    {
-        float mouseDeltaX = GetMouseAxis("Mouse X");
-        float mouseDeltaY = GetMouseAxis("Mouse Y");
+   private void UpdateRotation() {
+      float mouseDeltaX = GetMouseAxis("Mouse X");
+      float mouseDeltaY = GetMouseAxis("Mouse Y");
 
-        xRotation = Mathf.Clamp(xRotation - mouseDeltaY, -90f, 90f);
+      xRotation = Mathf.Clamp(xRotation - mouseDeltaY, -90f, 90f);
 
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        player.Rotate(Vector3.up * mouseDeltaX);
-    }
+      transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+      player.Rotate(Vector3.up * mouseDeltaX);
+   }
 
-    private float GetMouseAxis(string axis)
-    {
-        return Input.GetAxis(axis) * mouseSensitivity * Time.deltaTime;
-    }
+   private float GetMouseAxis(string axis) {
+      return Input.GetAxis(axis) * mouseSensitivity * Time.deltaTime;
+   }
 }
